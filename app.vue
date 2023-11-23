bd5fcc915847ad971a359d345bcd234fc82a453e
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2023-11-24 01:26:26 +0700

    added search base

--- app.vue
+++ app.vue
@@ -1,5 +1,59 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <v-container>
+        <center>
+          <h1>Cooler Cosmart</h1>
+        </center>
+        <v-text-field density="compact" placeholder="What are you thinking?" prepend-inner-icon="mdi-magnify"
+          variant="outlined" @change="showCards"></v-text-field>
+        <center>
+          <v-row>
+            <v-col v-bind:key="item.name" v-for="item in items" align-self="center">
+              <ItemCard :productName="item.name" :category="item.category" :brand="item.brand" :price="item.price" />
+            </v-col>
+          </v-row>
+        </center>
+      </v-container>
+    </v-app>
+  </NuxtLayout>
 </template>
+
+<script lang="ts">
+
+export default {
+  data: () => {
+    return {
+      categories: ["Electronic", "Skincare", "Toy"],
+      items: [
+        {
+          name: "Redmi 69420",
+          brand: "Xiaomi",
+          category: "Electronic",
+          price: 30
+        },
+        {
+          name: "Pocopon 6969",
+          brand: "Xiaomi",
+          category: "Electronic",
+          price: 20
+        },
+        {
+          name: "Samsung Galaxy Z Fold 69",
+          brand: "Samsung",
+          category: "Electronic",
+          price: 69
+        },
+      ]
+    }
+  },
+  mounted: () => {
+
+  },
+  methods: {
+    showCards() {
+
+    }
+  }
+}
+</script>
---
+++ components/ItemCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card width="400">
+        <v-card-item>
+            <v-card-title>{{ productName }}</v-card-title>
+
+            <v-card-subtitle>Category: {{ category }}</v-card-subtitle>
+            <v-card-subtitle>Brand: {{ brand }}</v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+            Price: {{ price }}
+        </v-card-text>
+    </v-card>
+</template>
+
+<script lang="ts">
+
+export default {
+    props: ["productName", "category", "brand", "price"]
+}
+</script>
